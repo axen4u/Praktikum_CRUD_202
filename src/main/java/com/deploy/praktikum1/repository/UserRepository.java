f5e8d99266a64170a7bdff00a99f06a5a8cbb32f
axen4u <damaraarya1303@gmail.com>
2026-03-04 11:10:32 +0700

    7. Add UserRepository interface extending JpaRepository

---
+++ src/main/java/com/deploy/praktikum1/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.deploy.praktikum1.repository;
+
+import com.deploy.praktikum1.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByName(String name);
+}
